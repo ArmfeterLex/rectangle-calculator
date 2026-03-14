3110be830a0d7e5712bd700b60c753f199b66557
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-15 00:41:19 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите стороны прямоугольника");
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double p = Math.Round((a + b) * 2, 2); double s = Math.Round(a*b, 2);
+        Console.WriteLine("Периметр прямоугольника {0:f2}; Площадь прямоугольника {1:f2}", p, s);
+        Console.ReadKey();
+    }
+}
